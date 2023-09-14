9912c0ffb980018ad3a3fc2375c580e471943252
Lia Kahn <liakahn@Lias-MacBook-Pro.local>
2023-09-14 14:43:42 -0600

    added starter code

---
+++ index.js
@@ -0,0 +1,13 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
