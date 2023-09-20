b427b15fce035a3f4eca48eba74a3d2a04f82eb9
Lia Kahn <liakahn@Lias-MacBook-Pro.local>
2023-09-19 20:02:45 -0600

    updated license badge function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,7 +4,7 @@ function renderLicenseBadge(license) {
   if (license === 'MIT') {
     return '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]';
   } else if (license === 'Apache 2.0') {
-    return ' ![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)]';
+    return ' ![License: Apache 2.0](https://img.shields.io/badge/License-Apache_2.0-blue.svg)]';
   } else if (license === 'GNU') {
     return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]';
   } else {
